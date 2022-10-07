5fddaa24df9a9eb2379a5b61e49f7b354f8dab62
rexmalebka <rexmalebka@krutt.org>
2022-10-06 21:02:22 -0500

    implemented group query

--- src/sc_group.erl
+++ src/sc_group.erl
@@ -4,6 +4,7 @@
 
 -export([
 	 add/2, add/3
+	 , get/1
 	 , remove/2, remove/1	
 	]).
 
@@ -37,6 +38,85 @@ add(OSC, GroupId, {replace, TargetAction}) ->  add(OSC, GroupId, {4, TargetActio
 add(GroupId, {AddAction, TargetAction}) ->
 	  OSC = sc:get_client(),
 	  add(OSC, GroupId, {AddAction, TargetAction}).
+
+-type querySynthArgs() :: { string() | integer(), float() | string() }.
+
+-type  querySynth() :: {synth, SynthId::integer(), SynthDefName::string(), [querySynthArgs()] }.
+
+-type  queryTree() :: {group, GroupId::integer(), [querySynth()] }.
+
+-spec get(GroupIp::integer() ) -> queryTree().
+
+get(GroupId) when is_integer(GroupId)->
+	OSC = sc:get_client(),
+	case  osc_client:call_msg(
+	  OSC
+	  , "/g_queryTree",
+	  [GroupId, 1] ) of
+		{message, "/fail",_} -> {error, {group, not_found}};
+		{message, "/g_queryTree.reply", QueryTree} -> 
+		       parse_queryTree(QueryTree)
+	end.
+
+parse_queryTree([HasSynthControl, GroupId, NumChildNodes | ChildNodes ]) when 
+	  HasSynthControl == 1
+	  ->
+	QuerySynths = parse_querySynth(ChildNodes, [], NumChildNodes),
+	{
+	 group,
+	 GroupId,
+	 QuerySynths
+	}.
+
+parse_querySynth([NodeId, ChildNodesNumber  |ChildNodes], QueryList, NumNodes) when ( NumNodes > 0 ) ->
+	{Output, RestChildNodes}  = case  <<ChildNodesNumber>> == <<-1>> of
+			 true ->
+				 [ SynthDefName, NumControls| RestData ] = ChildNodes,
+				 {ControlArgsList, Rest} = lists:split(NumControls*2, RestData),
+				 
+
+				 {_, ControlArgsPList} = lists:foldl(fun(Elem, {odd, Acc} ) ->
+							     {even, Acc, Elem};
+						(Elem, {even, Acc, Prop}) ->
+							     {odd, Acc ++ [ {Prop, Elem} ]}
+					     end
+					    , {odd, []}
+					    , ControlArgsList
+					    ),
+
+				 ControlArgs = maps:from_list(ControlArgsPList),
+				 {
+				  {
+				   synth,
+				   NodeId,
+ 				   SynthDefName,
+ 				   ControlArgs
+ 				  },
+				  Rest
+				 };
+
+			 false ->
+						    {
+				 {
+				 group,
+				 NodeId,
+				 ChildNodesNumber
+				 },
+				 ChildNodes}
+		 end,
+	parse_querySynth(RestChildNodes, QueryList ++ [Output], NumNodes -1 );
+
+parse_querySynth(_ChildNodes, QueryList, NumNodes) when ( NumNodes == 0 ) ->
+	QueryList.
+
+
+
+
+
+
+
+
+
 
 -spec remove(OSC::pid(), GroupId::integer()) -> ok.
 
